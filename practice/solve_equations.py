eea52d747e9e4a094417044927ad3451c75120dc
AlexWei <ty.wei@foxmail.com>
2020-03-20 08:27:15 +0800

    clac sth else

--- practice/solve_equations.py
+++ practice/solve_equations.py
@@ -3,17 +3,8 @@ import time
 
 # "SEND + MORE = MONEY"
 
-S = Symbol("S")
-E = Symbol("E")
-N = Symbol("N")
-D = Symbol("D")
-M = Symbol("M")
-O = Symbol("O")
-R = Symbol("R")
-Y = Symbol("Y")
+x = Symbol('x')
 
-result = solve(1000*S + 100*E + 10*N + D + 1000*M + 100*O + 10*R + E - 10000*M - 1000*O - 100*N - 10*E - Y,S)
-print("solving...")
-time.sleep(3)
+result = solve(pow(x, 3) - 1, x)
 
 print("the answer is:" + str(result))
